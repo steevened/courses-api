f86cf2603d6f29ce88206c644e98b46157db3048
steven <alvaradosteven315@gmail.com>
2023-01-13 18:44:11 -0500

    adding courses seed

--- src/seeders/seed.js
+++ src/seeders/seed.js
@@ -1,6 +1,7 @@
 const db = require('../utils/database')
 const initModels = require('../models/init.models')
 const Users = require('../models/users.models')
+const Courses = require('../models/courses.models')
 
 initModels()
 
@@ -30,9 +31,36 @@ const users = [
     email: 'diego@g.com',
   },
 ]
+
+const courses = [
+  {
+    title: 'nodejs',
+    description: 'last course',
+    instructor: 'Rosas',
+  },
+  {
+    title: 'react',
+    description: 'front course',
+    instructor: 'Andres',
+  },
+  {
+    title: 'foundations',
+    description: 'first course',
+    instructor: 'Rosas',
+  },
+  {
+    title: 'python',
+    description: 'post course',
+    instructor: 'unknown',
+  },
+]
+
 db.sync({ force: true })
   .then(() => {
     console.log('seeding')
     users.forEach((user) => Users.create(user))
+    setTimeout(() => {
+      courses.forEach((course) => Courses.create(course))
+    }, 100)
   })
   .catch((error) => console.log(error))
